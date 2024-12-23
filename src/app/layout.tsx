8d92613c85cd47bfee2551ef897706e282c9b58b
maxitect <maxime.downe@gmail.com>
2024-12-23 15:05:13 +0000

    style: update layout.tsx to use lora font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Inter } from 'next/font/google'
-import { Providers } from '@/context/providers'
+import { Lora } from "next/font/google";
+import { Providers } from "@/context/providers";
 
-const inter = Inter({ subsets: ['latin'] })
+const lora = Lora({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Steph & Max Wedding',
-  description: 'Join us in celebrating our special day - October 18, 2025',
-}
+  title: "Steph & Max Wedding",
+  description: "Join us in celebrating our special day - October 18, 2025",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={lora.className}>
+        <div className="infinity-watermark">∞</div>
         <Providers>{children}</Providers>
       </body>
     </html>
-  )
+  );
 }
-
