e237bfa0e1492ac740b36f24a5bdb81bcdddf8fe
maxitect <maxime.downe@gmail.com>
2024-12-23 14:13:51 +0000

    chore: update to providers.tsx

--- src/context/providers.tsx
+++ src/context/providers.tsx
@@ -1,14 +1,12 @@
-'use client'
+"use client";
 
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
-import { ParallaxProvider } from 'react-scroll-parallax'
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ChakraProvider value={defaultSystem}>
-      <ParallaxProvider>
-        {children}
-      </ParallaxProvider>
+      <ParallaxProvider>{children}</ParallaxProvider>
     </ChakraProvider>
-  )
+  );
 }
